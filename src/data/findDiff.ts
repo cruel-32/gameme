66c5734b577a4d26a3df3ba0d6f8140eee62a016
cruel32 <cruel32@nate.com>
2020-10-24 23:22:17 +0900

    - 이미지 업데이트 준비중... - DM 보내서 허락부터 받아보자 ㅠ

--- src/data/findDiff.ts
+++ src/data/findDiff.ts
@@ -85,13 +85,13 @@ export const imageData: IFindDiffImageData[] = [
     ],
   },
   {
-    img: '/images/findDiff/s2.ssh6.jpg',
-    diffImg: '/images/findDiff/s2.ssh6_diff.jpg',
+    img: '/images/findDiff/e.yoonjin1.jpg',
+    diffImg: '/images/findDiff/e.yoonjin1_diff.jpg',
     description:
       '아름답네요. 그러나 테스트용 이미지라 곧 지워야합니다. ㅠ ㅠ 지워진 이후에는 그녀의 인스타에서 보세요',
-    url: 'https://www.instagram.com/s2.ssh',
+    url: 'https://www.instagram.com/e.yoonjin',
     tags: ['어쩌구1', '저쩌구1', '하하하1'],
-    author: 's2.ssh',
+    author: 'e.yoonjin',
     diffPoints: [
       {
         top: '79%',
@@ -102,13 +102,13 @@ export const imageData: IFindDiffImageData[] = [
     ],
   },
   {
-    img: '/images/findDiff/s2.ssh7.jpg',
-    diffImg: '/images/findDiff/s2.ssh7_diff.jpg',
+    img: '/images/findDiff/s0on_ho1.jpg',
+    diffImg: '/images/findDiff/s0on_ho1_diff.jpg',
     description:
       '아름답네요. 그러나 테스트용 이미지라 곧 지워야합니다. ㅠ ㅠ 지워진 이후에는 그녀의 인스타에서 보세요',
-    url: 'https://www.instagram.com/s2.ssh',
+    url: 'https://www.instagram.com/s0on_ho',
     tags: ['어쩌구1', '저쩌구1', '하하하1'],
-    author: 's2.ssh',
+    author: 's0on_ho',
     diffPoints: [
       {
         top: '79%',
